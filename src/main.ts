0e61e79a964341c73b4fdc78cdd744ecf8084122
yamazakira <yamazaki.akiravl@gmail.com>
2024-09-09 22:50:32 -0300

    Alteração config vercel - teste 2

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
     //.addTag('ToDo')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
