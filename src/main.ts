3ded473d44ac6e5cede8c2c3c502e5a14d88dbbc
yamazakira <yamazaki.akiravl@gmail.com>
2024-09-09 22:45:52 -0300

    Mudança servidor, swagger

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,20 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
+  
   const config = new DocumentBuilder()
     .setTitle('ToDo List API')
     .setDescription('Simple To-do list API')
     .setVersion('1.0')
-    .addTag('ToDo')
+    //.addTag('ToDo')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
